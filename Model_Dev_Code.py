152ace1f9893644f6ba7a3d28f3b3029388c9a35
Reshmitha tc <reshmithatc@gmail.com>
2026-07-13 20:03:08 +0530

    Update Model_Dev_Code.py

--- Model_Dev_Code.py
+++ Model_Dev_Code.py
@@ -16,7 +16,6 @@ from sklearn.neighbors import KNeighborsClassifier
 
 import pickle
 
-
 # ===================== LOAD DATA =====================
 file_path = "AI_Brosnan_CancerDataset.xlsx"
 df = pd.read_excel("AI_Brosnan_CancerDataset.xlsx", sheet_name="Cleaned_Data")
